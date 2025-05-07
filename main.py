fc1f4faa9b2cfaeb94ab86d04080dcbb5d88e2ba
ProdigyJordan <jordan@prodigypartners.co>
2025-05-07 17:31:49 -0600

    adding chroma cli and main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main.py
-import chromadb.server
+from chromadb.server.fastapi import run
 
 if __name__ == "__main__":
-    chromadb.server.run(host="0.0.0.0", port=8000, path="/data")
+    run(host="0.0.0.0", port=8000, path="/data")
